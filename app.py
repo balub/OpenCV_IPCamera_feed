eb8df777b6c5662799b9ea2e15f4a5c0e236b51a
Balu Babu <balub997@gmail.com>
2020-01-14 16:13:43 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+url = 'rtsp://admin:admin123@192.168.1.6/1'
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture()
+cap.open(url)
+
+while(True):
+     # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('Salida',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
